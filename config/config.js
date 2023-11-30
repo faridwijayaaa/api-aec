def2ce5f0d74a27586ac449f7f62e0a2cee2fb4e
faridwijayaaa <faridwijayaaa@gmail.com>
2023-11-30 10:52:45 +0700

    prod

--- config/config.js
+++ config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.PROD_DB_USER,
+    password: process.env.PROD_DB_PASS,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOST,
+    dialect: process.env_PROD_DB,
   },
 };
